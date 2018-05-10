ed00a0b3fc4f9d34071f376e7ce6c6df51a5bc27
madzathemes <madza@madzathemes.com>
2018-05-11 01:12:15 +0300

    fix

--- frontend/shortcode.php
+++ frontend/shortcode.php
@@ -328,7 +328,7 @@ class WOOMULTI_CURRENCY_F_Frontend_Shortcode {
 						?>
 						<div class="wmc-currency <?php echo esc_attr( $class ) ?>">
 							<a title="<?php echo esc_attr( $country['name'] ) ?>" href="<?php echo $link ?>">
-								<span class="flags flags-<?php echo $country['code']; ?>"><img width="30" height="20"  src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-lazy-type="image" data-src="<?php echo get_template_directory_uri(); ?>/assets/images/flag_<?php echo $country['code']; ?>.png"/></span>
+								<span class="flags flags-<?php echo $country['code']; ?>"><img class="lazy" width="30" height="20"  src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-lazy-type="image" data-src="<?php echo get_template_directory_uri(); ?>/assets/images/flag_<?php echo $country['code']; ?>.png"/></span>
 								<span>
 									<?php echo esc_html( $country['name'] ) ?>
 								</span>
